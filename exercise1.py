024c1612c8c12ce8dcd24a2c5fe11bbb5d96bf7b
Yokonomore <118581649+Yokonomore@users.noreply.github.com>
2023-01-25 01:31:52 +0800

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,27 @@
+# find year that will turn 100
+
+age = int(input("Enter your age: "))
+year = 2022
+oldyear = (year-age) + 100
+print("This is 2022. You will turn 100 in", oldyear) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
